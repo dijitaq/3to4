e60d2d77b8fea42364f46d1a44b665f51024768d
firdaus riyanto <firdaus@madewow.com>
2018-07-19 15:29:10 +0700

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
 $(document).foundation();
+
+$(document).ready(function(){
+	$('#team-slider').slick({
+		prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+	})
+});
