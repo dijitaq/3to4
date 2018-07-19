5ebf9ab92004237423f99526fa52749e669b230b
firdaus riyanto <firdaus@madewow.com>
2018-07-19 16:00:40 +0700

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,45 @@ $(document).ready(function(){
 		nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
 	})
 });
+
+$('form').submit(function(ev) {
+	ev.preventDefault();
+	
+}).on('invalid.zf.abide', function(ev, elm){
+	console.log(ev);
+		
+}).on('formvalid.zf.abide', function(ev, elm){
+	var thisForm = $(ev.target);
+	var name = $('input[name=name]').val();
+	var email = $('input[name=email]').val();
+	
+    thisForm.addClass('sending');
+	thisForm.find('input').attr('disabled', true);
+	thisForm.find('textarea').attr('disabled', true);
+	thisForm.find('button').attr('disabled', true);
+	thisForm.find('button').find('span').text("sending");
+	
+	/*$.ajax({
+        type: 'POST',
+        url: 'subscribe.php', //
+        data: { nname: nname, email: email },
+        success: function(response){
+	        console.log(response);
+        	response=JSON.parse(response);
+        	
+        	if (response) {
+	        	thisForm.find('.callout').addClass('show');
+	        	thisForm.removeClass('sending');
+		        thisForm.find('input').attr('disabled', false).val('').prop('checked', false);
+		        thisForm.find('button').attr('disabled', false);
+		        	
+	        	if (response.success == 1) {
+		        	thisForm.find('.callout').find('p').text(response.message);
+		        	
+				} else {
+					thisForm.find('.callout').find('p').text(response.message);
+				}
+			}
+        }
+	});*/
+});
