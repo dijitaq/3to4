5da286959bb15171542cb8ee7b294d6c6dfed769
firdaus riyanto <firdaus@madewow.com>
2018-07-27 07:23:24 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,111 +1,111 @@
 $(document).foundation();
 
 $(document).ready(function(){
-	$('#team-slider').slick({
-		prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
-		nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
-	});
+    // what we do section
+    var scene_0_controller = new ScrollMagic.Controller();
+    var scene_0 = new ScrollMagic.Scene({
+        triggerElement: '#hero-image',
+        triggerHook: "onLeave",
+        duration: "100%"
+    })
+    .addTo(scene_0_controller);
+
+    // what we do section
+    var scene_1_controller = new ScrollMagic.Controller();
+    var scene_1 = new ScrollMagic.Scene({
+        triggerElement: '#what-we-do',
+        //triggerHook: "onEnter",
+        duration: "100%"
+    })
+    //.addIndicators()
+    .on("enter", function(){
+        $('#what-we-do').addClass('is-in-view');
+    })
+    .addTo(scene_1_controller);
+
+    // customer section
+    var scene_2_controller = new ScrollMagic.Controller();
+    var scene_2 = new ScrollMagic.Scene({
+        triggerElement: '#customer',
+        //triggerHook: "onEnter",
+        duration: 2000
+    })
+    //.addIndicators()
+    .on("enter", function(){
+        $('#customer').addClass('is-in-view');
+    })
+    .addTo(scene_2_controller);
     
     if (Foundation.MediaQuery.atLeast('large')) {
-        // what we do section
-        var scene_0_controller = new ScrollMagic.Controller();
-        var scene_0 = new ScrollMagic.Scene({
-            triggerElement: '#hero-image',
-            triggerHook: "onLeave",
-            duration: "100%"
-        })
-        .addTo(scene_0_controller);
-
-        // what we do section
-        var scene_1_controller = new ScrollMagic.Controller();
-        var scene_1 = new ScrollMagic.Scene({
-            triggerElement: '#what-we-do',
-            //triggerHook: "onEnter",
-            duration: "100%"
-        })
-        //.addIndicators()
-        .on("enter", function(){
-            $('#what-we-do').addClass('is-in-view');
-        })
-        .addTo(scene_1_controller);
-
-        // customer section
         var tween_2 = new TimelineMax()
         .add([
-            TweenMax.to('#customer', 1, { backgroundPosition: "right -600px", force3D:true, ease: Linear.easeNone })
+            TweenMax.to('#customer', 1, { backgroundPosition: "100% -300px", force3D:true, ease: Linear.easeNone })
         ]);
+        
+        scene_2.setTween(tween_2);
+    }
 
-        var scene_2_controller = new ScrollMagic.Controller();
-        var scene_2 = new ScrollMagic.Scene({
-            triggerElement: '#customer',
-            //triggerHook: "onEnter",
-            duration: 2000
-        })
-        .setTween(tween_2)
-        //.addIndicators()
-        .on("enter", function(){
-            $('#customer').addClass('is-in-view');
-        })
-        .addTo(scene_2_controller);
-
+    var scene_3_controller = new ScrollMagic.Controller();
+    var scene_3 = new ScrollMagic.Scene({
+        triggerElement: '#our-story',
+        //triggerHook: "onLeave",
+        duration: 2000
+    })
+    //.addIndicators()
+    .on("enter", function(){
+        $('#our-story').addClass('is-in-view');
+    })
+    .addTo(scene_3_controller);
+    
+    if (Foundation.MediaQuery.atLeast('large')) {
         // our story section
         var tween_3 = new TimelineMax()
         .add([
-            TweenMax.to('#our-story', 1, { backgroundPosition: "0 -300px", force3D:true, ease: Linear.easeNone })
+            TweenMax.to('#our-story', 1, { backgroundPosition: "0 -200px", force3D:true, ease: Linear.easeNone })
         ]);
-
-        var scene_3_controller = new ScrollMagic.Controller();
-        var scene_3 = new ScrollMagic.Scene({
-            triggerElement: '#our-story',
-            //triggerHook: "onLeave",
-            duration: 2000
-        })
-        .setTween(tween_3)
-        //.addIndicators()
-        .on("enter", function(){
-            $('#our-story').addClass('is-in-view');
-        })
-        .addTo(scene_3_controller);
-
-        // our team section
-        var scene_4_controller = new ScrollMagic.Controller();
-        var scene_4 = new ScrollMagic.Scene({
-            triggerElement: '#our-team',
-            //triggerHook: "onEnter",
-            duration: "100%"
-        })
-        //.addIndicators()
-        .on("enter", function(){
-            $('#our-team').addClass('is-in-view');
-        })
-        .addTo(scene_4_controller);
-
-        // our partners section
-        var scene_5_controller = new ScrollMagic.Controller();
-        var scene_5 = new ScrollMagic.Scene({
-            triggerElement: '#our-partners',
-            //triggerHook: "onEnter",
-            duration: "100%"
-        })
-        //.addIndicators()
-        .on("enter", function(){
-            $('#our-partners').addClass('is-in-view');
-        })
-        .addTo(scene_5_controller);
-
-        // contact us section
-        var scene_6_controller = new ScrollMagic.Controller();
-        var scene_6 = new ScrollMagic.Scene({
-            triggerElement: '#contact-us',
-            //triggerHook: "onEnter",
-            duration: "100%"
-        })
-        //.addIndicators()
-        .on("enter", function(){
-            $('#contact-us').addClass('is-in-view');
-        })
-        .addTo(scene_6_controller);
+        
+        scene_3.setTween(tween_3);
     }
+
+    // our team section
+    var scene_4_controller = new ScrollMagic.Controller();
+    var scene_4 = new ScrollMagic.Scene({
+        triggerElement: '#our-team',
+        //triggerHook: "onEnter",
+        duration: "100%"
+    })
+    //.addIndicators()
+    .on("enter", function(){
+        $('#our-team').addClass('is-in-view');
+    })
+    .addTo(scene_4_controller);
+
+    // brand experience section
+    var scene_5_controller = new ScrollMagic.Controller();
+    var scene_5 = new ScrollMagic.Scene({
+        triggerElement: '#brand-experience',
+        //triggerHook: "onEnter",
+        duration: "100%"
+    })
+    //.addIndicators()
+    .on("enter", function(){
+        $('#brand-experience').addClass('is-in-view');
+    })
+    .addTo(scene_5_controller);
+
+    // contact us section
+    var scene_6_controller = new ScrollMagic.Controller();
+    var scene_6 = new ScrollMagic.Scene({
+        triggerElement: '#contact-us',
+        //triggerHook: "onEnter",
+        duration: "100%"
+    })
+    //.addIndicators()
+    .on("enter", function(){
+        $('#contact-us').addClass('is-in-view');
+    })
+    .addTo(scene_6_controller);
+    
 	// partners section
 	if ($('.logos').length > 0) {
 		$('.logos').each(function(){
@@ -127,10 +127,6 @@ $(document).ready(function(){
 	}
 });
 
-$('#what-we-do-content').on('replaced.zf.interchange', function(){
-    $(document).foundation();
-});
-
 //
 $('form').submit(function(ev) {
 	ev.preventDefault();
@@ -142,6 +138,7 @@ $('form').submit(function(ev) {
 	var thisForm = $(ev.target);
 	var name = $('input[name=name]').val();
 	var email = $('input[name=email]').val();
+    var message = $('textarea[name=message]').val();
 	
     thisForm.addClass('sending');
 	thisForm.find('input').attr('disabled', true);
@@ -149,34 +146,30 @@ $('form').submit(function(ev) {
 	thisForm.find('button').attr('disabled', true);
 	thisForm.find('button').find('span').text("sending");
 	
-	/*$.ajax({
+	$.ajax({
         type: 'POST',
-        url: 'subscribe.php', //
-        data: { nname: nname, email: email },
+        url: 'submit-email.php', //
+        data: { name: name, email: email, message:message },
         success: function(response){
 	        console.log(response);
         	response=JSON.parse(response);
         	
         	if (response) {
-	        	thisForm.find('.callout').addClass('show');
+	        	thisForm.find('.callout').removeClass('hide');
 	        	thisForm.removeClass('sending');
 		        thisForm.find('input').attr('disabled', false).val('').prop('checked', false);
+                thisForm.find('textarea').attr('disabled', false).val('').prop('checked', false);
 		        thisForm.find('button').attr('disabled', false);
-		        	
+		      
+                console.log(response.message);
+                
 	        	if (response.success == 1) {
-		        	thisForm.find('.callout').find('p').text(response.message);
+		        	thisForm.find('.callout').addClass('success').find('p').text(response.message);
 		        	
 				} else {
-					thisForm.find('.callout').find('p').text(response.message);
+					thisForm.find('.callout').addClass('alert').find('p').text(response.message);
 				}
 			}
         }
-	});*/
-});
-
-$('.btn-goto').on('click', function(e){
-	e.preventDefault();
-	var slide = Number($(this).attr('data-goto'));
-	
-	$('#team-slider').slick('slickGoTo', slide);
+	});
 });
---
+++ submit-email.php
@@ -0,0 +1,54 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'vendor/autoload.php';
+
+	if (!isset($_POST['email'])){
+		die('Restricted access');
+		$return['success'] = 0;
+		echo json_encode($return);
+	}
+	$mail = new PHPMailer();
+	$mail->IsSMTP();
+	$mail->CharSet = 'UTF-8';
+
+	// $mail->Host       = "smtp.zoho.com"; // SMTP server example
+	// $mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
+	// $mail->SMTPAuth   = true;                  // enable SMTP authentication
+	// $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+	// $mail->Port       = 465;                    // set the SMTP port for the GMAIL server
+	// $mail->Username   = "mailer@stompingit.com"; // SMTP account username example
+	// $mail->Password   = "stompingit#54321";        // SMTP account password example
+
+	// Godady Setting
+		$mail->Host = 'sg2plcpnl0034.prod.sin2.secureserver.net';
+		$mail->Port = 465;
+		$mail->SMTPAuth = true;
+		$mail->SMTPSecure = 'ssl';
+		$mail->Username   = "no-spam@threetofour.com.au";
+		$mail->Password   = "firDaus&3Mw";
+
+	$mail->isHTML(false);                                  // Set email format to HTML
+
+	$mail->setFrom('no-spam@threetofour.com.au', 'ThreeToFour website mailer');
+
+	//$mail->addAddress('jay@damonxlabs.com');
+	//$mail->addAddress('amber@damonxlabs.com');
+	$mail->addAddress('no-spam@threetofour.com.au');
+    $mail->addReplyTo($_POST['email'], $_POST['name']);
+	//$email = $_POST['email'];
+
+	$mail->Subject = 'new message from ' . $_POST['name'];
+	$mail->Body = $_POST['message'];
+
+	if($mail->send()) {
+		$return['success'] = 1;
+	}
+	else{
+		$return['success'] = 0;
+		$return['errorinfo'] = $mail->ErrorInfo;
+	}
+	// $return['success'] = 1;
+	echo json_encode($return);
+?>
